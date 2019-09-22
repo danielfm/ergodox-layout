5f29f1182ffa57f69a172c18cf9635b5d3c75f7c
Daniel Martins <daniel.tritone@gmail.com>
2019-09-22 18:47:12 -0300

    Use right shift at the right half

--- keymap.c
+++ keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_LBRC, KC_RBRC, KC_UP,   KC_RGHT,          KC_LGUI,
         KC_RGUI, RALT(KC_COMM),
         OSM(MOD_RALT),
-        OSM(MOD_LALT), LSFT_T(KC_ENTER), LCTL_T(KC_SPC)
+        OSM(MOD_LALT), RSFT_T(KC_ENTER), LCTL_T(KC_SPC)
     ),
 
 /* Keymap 1: Symbol Layer
